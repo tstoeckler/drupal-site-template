f22994abb35480489ce166f0004196f29b3dbdf2
Florian Weber <florian@webflo.org>
2015-01-12 11:34:59 +0100

    Update drushrc.php

--- web/drush/drushrc.php
+++ web/drush/drushrc.php
@@ -2,6 +2,5 @@
 
 // Make Drush use the project's autoloader instead of Drupal core's hardcoded
 // one.
-print 'foo' . PHP_EOL;
 $autoloader = require __DIR__ . '/../../vendor/autoload.php';
 drush_drupal_load_autoloader(NULL, $autoloader);
