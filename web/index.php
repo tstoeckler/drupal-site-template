8ded80e820e17a2d96ad14e2ab99fef3316ecda9
Tobias Stöckler <tobiasstoeckler@googlemail.com>
2015-01-14 01:58:55 +0100

    Update index.php to use the generated autoloader

--- web/index.php
+++ web/index.php
@@ -3,16 +3,13 @@
 /**
  * @file
  * The PHP page that serves all page requests on a Drupal installation.
- *
- * All Drupal code is released under the GNU General Public License.
- * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
  */
 
 use Drupal\Core\DrupalKernel;
 use Drupal\Core\Site\Settings;
 use Symfony\Component\HttpFoundation\Request;
 
-$autoloader = require_once __DIR__ . '/core/vendor/autoload.php';
+$autoloader = require_once __DIR__ . '/../vendor/autoload.php';
 
 try {
 
